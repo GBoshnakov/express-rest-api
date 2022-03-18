ffbe093bd5d3c825d4adf60024cf412d57586eda
GBoshnakov <boshnakov.g@gmail.com>
2022-03-18 11:36:27 +0200

    added furniture controller and service

--- controllers/furniture.js
+++ controllers/furniture.js
@@ -1,11 +1,33 @@
 const router = require('express').Router();
+const furnitureService = require('../services/furnitures');
 
-router.get('/', (req, res) => {
-    res.json({ msg: 'smt' });
+router.get('/', async (req, res) => {
+    try {
+        const result = await furnitureService.getAll();
+        res.json(result); 
+    } catch (error) {
+        console.error(error);
+    }
 });
 
-router.post('/', (req, res) => {
-    console.log(req.body);
+router.post('/', async (req, res) => {
+
+    const furniture = {
+        make: req.body.make,
+        model: req.body.model,
+        year: Number(req.body.year),
+        description: req.body.description,
+        price: Number(req.body.price),
+        img: req.body.img,
+        material: req.body.material
+    };
+
+    try {
+        const result = await furnitureService.createNew(furniture);
+        res.json(result);
+    } catch (error) {
+        console.error(error);
+    }
 })
 
 module.exports = router;
--- services/furnitures.js
+++ services/furnitures.js
@@ -1,1 +1,23 @@
 const Furniture = require('../models/Furniture');
+
+async function getAll() {
+    return Furniture.find({});
+}
+
+async function getById(id) {
+    return Furniture.findById(id);
+}
+
+async function createNew(data) {
+    const furniture = new Furniture(data);
+    
+    await furniture.save();
+
+    return furniture;
+}
+
+module.exports = {
+    getAll,
+    getById,
+    createNew
+}
